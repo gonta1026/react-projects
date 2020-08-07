d805515a5a33fd7b29361150162c1736871e5ab6
gonta1026 <katano.original@gmail.com>
2020-08-08 00:06:26 +0900

    インデント修正

--- ec-app/src/reducks/users/actions.js
+++ ec-app/src/reducks/users/actions.js
@@ -1,27 +1,26 @@
 export const SIGN_IN = "SIGN_IN";
 export const signInAction = (userState) => {
-  console.log("actionsです")
-  return {
-    type: "SIGN_IN",
-    payload: {
-      role: userState.role,
-      isSigndIn: true,
-      uid: userState.uid,
-      username: userState.username
+    return {
+        type: "SIGN_IN",
+        payload: {
+            role: userState.role,
+            isSigndIn: true,
+            uid: userState.uid,
+            username: userState.username
+        }
     }
-  }
 }
 
 export const SIGN_OUT = "SIGN_OUT";
 export const signOutAction = () => {
-  return {
-    type: "SIGN_OUT",
-    payload: {
-      isSigndIn: false,
-      role: "",
-      uid: "",
-      username: ""
+    return {
+        type: "SIGN_OUT",
+        payload: {
+            isSigndIn: false,
+            role: "",
+            uid: "",
+            username: ""
+        }
     }
-  }
 }
 
