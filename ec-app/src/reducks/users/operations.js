9953458f9d8ce163d5c0f38506ec3ceb93bb27d6
gonta1026 <katano.original@gmail.com>
2020-08-08 12:54:18 +0900

    インデント修正

--- ec-app/src/reducks/users/operations.js
+++ ec-app/src/reducks/users/operations.js
@@ -1,23 +1,23 @@
-import {signInAction, signOutAction} from "./actions"
-import {push} from "connected-react-router";
-import {db, auth, firebaseTimeStamp} from "../../firebase/index"
+import { signInAction, signOutAction } from "./actions"
+import { push } from "connected-react-router";
+import { db, auth, firebaseTimeStamp } from "../../firebase/index"
 
 export const listenAuthState = (pathname) => {
   console.log(pathname)
   return async (dispatch) => {
     return auth.onAuthStateChanged(user => {
-      if (user){
-         const uid = user.uid;
-         db.collection("users").doc(uid).get()
-           .then(snapshot => {
-             const data = snapshot.data();
-             dispatch(signInAction({
-               role: data.role,
-               isSigndIn: true,
-               uid: uid,
-               username: data.username
-             }))
-           })
+      if (user) {
+        const uid = user.uid;
+        db.collection("users").doc(uid).get()
+          .then(snapshot => {
+            const data = snapshot.data();
+            dispatch(signInAction({
+              role: data.role,
+              isSigndIn: true,
+              uid: uid,
+              username: data.username
+            }))
+          })
       } else {
         dispatch(push("/signin"));
       }
@@ -28,7 +28,7 @@ export const listenAuthState = (pathname) => {
 export const signIn = (email, password) => {
   console.log("operationsです")
   return async (dispatch) => {
-    if ( email === "" || password === "") {
+    if (email === "" || password === "") {
       alert("入力もれがあります！");
       return false;
     }
@@ -37,23 +37,21 @@ export const signIn = (email, password) => {
       .then(result => {
         const user = result.user;
 
-        if (user){
+        if (user) {
           const uid = user.uid;
           db.collection("users").doc(uid).get()
-          .then(snapshot =>{
-            const data = snapshot.data();
-            dispatch(signInAction({
-              role: data.role,
-              isSigndIn: true,
-              uid: uid,
-              username: data.username
-            }))
-          })
+            .then(snapshot => {
+              const data = snapshot.data();
+              dispatch(signInAction({
+                role: data.role,
+                isSigndIn: true,
+                uid: uid,
+                username: data.username
+              }))
+            })
           dispatch(push("/"));
         }
       });
-
-    
   }
 }
 
@@ -64,59 +62,59 @@ export const signUp = (username, email, password, confirmPassword) => {
       alert("入力もれがあります！");
       return false;
     }
-    
+
     if (password !== confirmPassword) {
       alert("パスワードが一致していません！");
       return false;
     }
     auth.createUserWithEmailAndPassword(email, password)
-    .then((result)=>{
-      const user = result.user;
+      .then((result) => {
+        const user = result.user;
 
-      if (user){
-        const uid = user.uid;
-        const timeStamp = firebaseTimeStamp.now();
-        const initialUserData = {
-          created_at: timeStamp,
-          email: email,
-          username: username,
-          uid: uid,
-          updated_at: timeStamp,
-          role: "customer"
+        if (user) {
+          const uid = user.uid;
+          const timeStamp = firebaseTimeStamp.now();
+          const initialUserData = {
+            created_at: timeStamp,
+            email: email,
+            username: username,
+            uid: uid,
+            updated_at: timeStamp,
+            role: "customer"
+          }
+          db.collection("users").doc(uid).set(initialUserData)
+            .then(() => {
+              dispatch(push("/"));
+            });
         }
-        db.collection("users").doc(uid).set(initialUserData)
-          .then(()=> {
-            dispatch(push("/"));
-          });
-      }
-    });
+      });
   }
 }
 
 export const signOut = () => {
   return async (dispatch) => {
     auth.signOut()
-    .then(()=>{
-      dispatch(signOutAction());
-      dispatch(push("/signin"));
-    })
+      .then(() => {
+        dispatch(signOutAction());
+        dispatch(push("/signin"));
+      })
   }
 }
 
 export const resetPassword = (email) => {
   return async (dispatch) => {
-    if (email === ""){
+    if (email === "") {
       alert("入力がされていません！");
       return false;
     } else {
       auth.sendPasswordResetEmail()
-      .then(() => {
-        alert("登録のメールアドレスにパスワードリセット用のメールをお送りしました！");
-        dispatch(push("/signin"));
-      })
-      .catch(() =>{
-        alert("パスワードのリセットに失敗しました！");
-      });
+        .then(() => {
+          alert("登録のメールアドレスにパスワードリセット用のメールをお送りしました！");
+          dispatch(push("/signin"));
+        })
+        .catch(() => {
+          alert("パスワードのリセットに失敗しました！");
+        });
     }
   }
 }
