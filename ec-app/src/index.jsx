c9eef8ba9bf5e7151c9bc74308eb55a64bebdb3f
gonta1026 <katano.original@gmail.com>
2020-08-09 14:01:41 +0900

    material-uiで使うthemeファイルの読み込み

--- ec-app/src/index.jsx
+++ ec-app/src/index.jsx
@@ -6,13 +6,16 @@ import App from "./App";
 import { createBrowserHistory } from "history";
 import { unregister } from "./serviceWorker";
 import { ConnectedRouter } from "connected-react-router";
-
+import { MuiThemeProvider } from "@material-ui/core";
+import { theme } from "./assets/theme";
 const history = createBrowserHistory(); //各ページでの繊維履歴を持っている。
 export const getStore = store(history);
 ReactDOM.render(
     <Provider store={getStore}>
         <ConnectedRouter history={history}>
-            <App />
+            <MuiThemeProvider theme={theme}>
+                <App />
+            </MuiThemeProvider>
         </ConnectedRouter>
     </Provider>,
     document.getElementById("root")
