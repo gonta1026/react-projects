1f1431065b4de76d01c36a805098bacc0cf7d1f1
gonta1026 <katano.original@gmail.com>
2020-08-09 14:03:29 +0900

    詳細ページのサイズコンポーネントの作成

---
+++ ec-app/src/components/Products/SizeTable.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Table, TableCell, TableRow, TableBody, IconButton, TableContainer } from "@material-ui/core";
+
+import { ShoppingCart, FavoriteBorder } from '@material-ui/icons';
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    iconCell: {
+        padding: 0,
+        height: 48,
+        width: 48
+    }
+})
+
+const SizeTable = (props) => {
+    const classes = useStyles()
+    const sizes = props.sizes;
+    return (
+        <TableContainer>
+            <Table aria-label="simple table">
+                <TableBody>
+                    {sizes.length > 0 && (
+                        sizes.map((item) => (
+                            <TableRow key={item.size}>
+                                <TableCell component="th" scope="row">{item.size}</TableCell>
+                                <TableCell>残り{item.quantity}点</TableCell>
+                                <TableCell className={classes.iconCell}>
+                                    {item.quantity > 0 ? (
+                                        <IconButton
+                                            className={classes.iconCell}
+                                        >
+                                            <ShoppingCart />
+                                        </IconButton>
+                                    ) : (
+                                            <div>売切</div>
+                                        )}
+                                </TableCell>
+                                <TableCell className={classes.iconCell}>
+                                    <IconButton className={classes.iconCell}>
+                                        <FavoriteBorder />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default SizeTable;
