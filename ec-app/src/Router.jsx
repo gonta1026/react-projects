b9106283edd7117a2fbabb727e3b7abca366f2cd
gonta1026 <katano.original@gmail.com>
2020-08-09 14:07:39 +0900

    詳細ページの作成

--- ec-app/src/Router.jsx
+++ ec-app/src/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { SignIn, Home, SignUp, Reset, ProductEdit, ProductList } from "./templates";
+import { SignIn, Home, SignUp, Reset, ProductEdit, ProductList, ProductDetail } from "./templates";
 import Auth from "./Auth";
 
 const Router = () => {
@@ -8,9 +8,10 @@ const Router = () => {
         <Switch>
             <Route exact path={"/signin"} component={SignIn} />
             <Route exact path={"/signup"} component={SignUp} />
+            <Route exact path={"/signin/reset"} component={Reset} />
             <Auth>
-                <Route exact path={"/signin/reset"} component={Reset} />
                 <Route exact path={"(/)?"} component={ProductList} />
+                <Route exact path={"/product/:id"} component={ProductDetail} />
                 <Route path={"/product/edit(/:id)?"} component={ProductEdit} />
             </Auth>
         </Switch>
--- ec-app/src/templates/index.js
+++ ec-app/src/templates/index.js
@@ -1,5 +1,6 @@
 export { default as CartList } from "./CartList";
 export { default as Home } from "./Home";
+export { default as ProductDetail } from "./ProductDetail";
 export { default as ProductEdit } from "./ProductEdit";
 export { default as ProductList } from "./ProductList";
 export { default as Reset } from "./Reset";
