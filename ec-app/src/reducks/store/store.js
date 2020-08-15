87e8882bc856ff3d093ed8fca7ee78377b47ccf2
gonta1026 <katano.original@gmail.com>
2020-08-15 16:57:19 +0900

    redux-loggerの追加

--- ec-app/src/reducks/store/store.js
+++ ec-app/src/reducks/store/store.js
@@ -3,17 +3,26 @@ import { connectRouter, routerMiddleware } from "connected-react-router"
 import thunk from "redux-thunk";
 import { UsersReducer } from "../users/reducers"
 import { ProductsReducer } from "../products/reducers"
+import { createLogger } from 'redux-logger';
+
 
 export const store = (history) => {
+    const middlewares = [routerMiddleware(history), thunk]
+    if (process.env.NODE_ENV === "development") {
+        const logger = createLogger({
+            collapsed: true,
+            diff: true
+        });
+        middlewares.push(logger)
+        // [...middlewares, logger]
+    }
+
     return createStore(
         combineReducers({
             users: UsersReducer,
             products: ProductsReducer,
             router: connectRouter(history)
         }),
-        applyMiddleware(
-            routerMiddleware(history),
-            thunk
-        )
+        applyMiddleware(...middlewares)
     );
 }
